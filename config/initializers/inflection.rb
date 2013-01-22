00355efc28a8de7091819432ab20bac0f1559fd1
hylozero <hylozero@gmail.com>
2013-01-22 14:37:09 -0200

    Traducao Disciplinas

--- config/initializers/inflection.rb
+++ config/initializers/inflection.rb
@@ -9,6 +9,6 @@ ActiveSupport::Inflector.inflections do |inflect|
    inflect.irregular 'publicação', 'publicações'   
    inflect.irregular 'presença em palestra', 'presenças em palestras'   
    inflect.irregular 'edição ENEM', 'edições ENEM'   
-   inflect.irregular 'área de conhecimento', 'áreas de conhecimentos'   
+   inflect.irregular 'area de conhecimento', 'areas de conhecimentos'   
 end
 
